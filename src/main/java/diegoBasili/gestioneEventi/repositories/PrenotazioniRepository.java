c9566aa8b2549578a0c493520a0629e429fe1076
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-09-20 11:19:49 +0200

    add repositories

---
+++ src/main/java/diegoBasili/gestioneEventi/repositories/PrenotazioniRepository.java
@@ -0,0 +1,10 @@
+package diegoBasili.gestioneEventi.repositories;
+
+import diegoBasili.gestioneEventi.entities.Prenotazione;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface PrenotazioniRepository extends JpaRepository<Prenotazione, UUID> {
+}
---
+++ src/main/java/diegoBasili/gestioneEventi/repositories/UtentiRepository.java
@@ -0,0 +1,10 @@
+package diegoBasili.gestioneEventi.repositories;
+
+import diegoBasili.gestioneEventi.entities.Utente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface UtentiRepository extends JpaRepository<Utente, UUID> {
+}
